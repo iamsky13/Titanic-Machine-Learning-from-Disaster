4939e79dcfdce60bf4dd52a96007598c1700e1bc
Aakash <uniqueaakash13@hotmail.com>
2017-07-21 18:53:51 +0545

    changed data type of Pclass to factor

--- TitanicMachine Learning from Disaster/script.R
+++ TitanicMachine Learning from Disaster/script.R
@@ -6,4 +6,10 @@ test <- read.csv("test.csv", header = TRUE)
 
 #Since train has one variable more than test i.e. Survived, I created a new variable test.survived and added new variable/column Survived with default value None
 #rep replicates None value for total no. of rows in test and combined it with test data frame to make new data frame test.survived
-test.survived <- data.frame(Survived = rep("NONE",nrow(test)),test[,])
+test.survived <- data.frame(Survived = rep("NONE", nrow(test)), test[,])
+
+#Our both test.Survived and train has same variables we combine them both
+combine.data <- rbind(train, test.survived)
+
+#According to definition of Kaggle Pclass is Ticket class that gives socio economic status so it should not be integer you can check its structure using str(combine.data)
+combine.data$Pclass <- as.factor(combine.data$Pclass)
